eb0a92305a7bb1d1c0743d5fc0fe2bf5688975fb
charles <cgiguere@iname.com>
2025-03-29 10:18:47 -0400

    button widgets

--- JC4827W543_LVGLv9.ino
+++ JC4827W543_LVGLv9.ino
@@ -66,6 +66,20 @@ void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data)
   }
 }
 
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = (lv_obj_t *)lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -132,10 +146,20 @@ void setup()
     lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
     lv_indev_set_read_cb(indev, my_touchpad_read);
 
-    // Create a label to see if everything is working
-    lv_obj_t *label = lv_label_create(lv_screen_active());
-    lv_label_set_text(label, "Hello Arduino, I'm LVGL!(V" GFX_STR(LVGL_VERSION_MAJOR) "." GFX_STR(LVGL_VERSION_MINOR) "." GFX_STR(LVGL_VERSION_PATCH) ")");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    // Create some widgets to see if everything is working
+    lv_obj_t * title_label = lv_label_create(lv_screen_active());
+    lv_label_set_text(title_label, "Hello Arduino, I'm LVGL!(V" GFX_STR(LVGL_VERSION_MAJOR) "." GFX_STR(LVGL_VERSION_MINOR) "." GFX_STR(LVGL_VERSION_PATCH) ")");
+    lv_obj_align(title_label, LV_ALIGN_CENTER, 0, 0);
+    
+    lv_obj_t * btn = lv_button_create(lv_screen_active());  /*Add a button to the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                              /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                            /*Set its size*/
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);  /*Assign a callback to the button*/
+    
+    lv_obj_t * btn_label = lv_label_create(btn);            /*Add a label to the button*/
+    lv_label_set_text(btn_label, "Button");                 /*Set the label's text*/
+    lv_obj_center(btn_label);
+    
   }
 
   Serial.println("Setup done");
